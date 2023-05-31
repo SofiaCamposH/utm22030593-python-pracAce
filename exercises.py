d7d0940807d2e38c83b95f35ff00c539b38edd8a
Sofia Campos <sfabilene@gmail.com>
2023-05-31 12:07:31 -0500

    feat: tupple

---
+++ exercises.py
@@ -0,0 +1,13 @@
+#A list with 5 items, add a new element and print it using a for loop.
+Countrylist = ["Mexico", "United States", "Korea", "Brazil", "France"]
+Countrylist.append ("Canada")
+for item in Countrylist: 
+    print(item) 
+print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+#A tuple with 7 items and print it using a while loop.
+animals=("Cat","Dog","Cow","Rabbit","Duck","butterfly","Cricket")
+i=0 
+while i < len(animals):
+    print(animals[i])
+    i += 1
+
